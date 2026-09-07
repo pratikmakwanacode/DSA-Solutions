2e78f31939456e3525fcaa4279721026b3d94c9b
pratikmakwanacode <pratikmak8369@gmail.com>
2026-09-07 10:43:14 +0530

    Time: 1 ms (31.20%) | Memory: 11 MB (95.25%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.c
@@ -0,0 +1,26 @@
+int getValue(char c) {
+    if (c == 'I') return 1;
+    if (c == 'V') return 5;
+    if (c == 'X') return 10;
+    if (c == 'L') return 50;
+    if (c == 'C') return 100;
+    if (c == 'D') return 500;
+    if (c == 'M') return 1000;
+    return 0;
+}
+
+int romanToInt(char* s) {
+    int total = 0;
+    
+    for (int i = 0; s[i] != '\0'; i++) {
+        int curr = getValue(s[i]);
+        int next = getValue(s[i + 1]);
+        
+        if (curr < next)
+            total -= curr;
+        else
+            total += curr;
+    }
+    
+    return total;
+}
