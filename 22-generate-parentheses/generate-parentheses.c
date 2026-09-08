e3383a618f7c989d867c0ae8c8d07f0f86ab8db5
pratikmakwanacode <pratikmak8369@gmail.com>
2026-09-08 17:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (49.63%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+
+void backtrack(int n, int open, int close, char* current, int index, char** result, int* returnSize) {
+    if (index == 2 * n) {
+        current[index] = '\0';
+        result[*returnSize] = (char*)malloc((2 * n + 1) * sizeof(char));
+        strcpy(result[*returnSize], current);
+        (*returnSize)++;
+        return;
+    }
+
+    if (open < n) {
+        current[index] = '(';
+        backtrack(n, open + 1, close, current, index + 1, result, returnSize);
+    }
+    if (close < open) {
+        current[index] = ')';
+        backtrack(n, open, close + 1, current, index + 1, result, returnSize);
+    }
+}
+
+char** generateParenthesis(int n, int* returnSize) {
+    char** result = (char**)malloc(5000 * sizeof(char*));
+    char* current = (char*)malloc((2 * n + 1) * sizeof(char));
+    *returnSize = 0;
+    
+    backtrack(n, 0, 0, current, 0, result, returnSize);
+    
+    free(current);
+    return result;
+}
